9532b590a846cf469231d0a8605c395c8e0ca1b2
Mohammad Hossein Sedarat <mamaddd163@gmail.com>
2023-03-03 19:12:40 +0330

    compelet some options of orders table

--- src/screens/manager/ordersManagement/ordersManagement.tsx
+++ src/screens/manager/ordersManagement/ordersManagement.tsx
@@ -1,6 +1,6 @@
 import { getOrders } from "@/api"
-import { TextField } from "@/components"
-import { useEffect, useState } from "react"
+import moment from 'jalali-moment';
+import { useEffect, useRef, useState } from "react"
 import 'tailwindcss/tailwind.css'
 
 type dataObj= Record<string, unknown>
@@ -8,6 +8,11 @@ type dataObj= Record<string, unknown>
 
 export const OrderManagement = () => {
   const [orders, setOrders] = useState<dataObj[]>([])
+  const selectElement = useRef<HTMLSelectElement>(null)
+  const searchElement = useRef<HTMLInputElement>(null)
+  const [ordersArray, setOrdersArray] = useState<dataObj[]>()
+  const filterTemp: dataObj[] = []
+
 
 
     useEffect(() => {
@@ -18,21 +23,47 @@ export const OrderManagement = () => {
         ordersGetter()
     }, [])
 
+    const filtering = () => {
+        const searchTemp: dataObj[] = []
+        if (selectElement.current!.value !== 'all') {
+          orders.map((order) => selectElement.current!.value === order.delivered && filterTemp.push(order))
+          if (searchElement.current?.value) {
+            filterTemp.map((order) => `${order.username} ${order.lastname}`.includes(searchElement.current!.value) && searchTemp.push(order))
+          } else {
+            filterTemp.map((order) => searchTemp.push(order))
+          }
+        } else {
+          if (searchElement.current?.value) {
+            // console.log(searchElement.current.value)
+            orders.map((order) => `${order.username} ${order.lastname}`.includes(searchElement.current!.value) && searchTemp.push(order))
+          } else {
+            orders.map((order) => searchTemp.push(order))
+          }
+        }
+        setOrdersArray([...searchTemp])
+      }
+
   return (
     <div className="relative p-10 pt-16">
         <b className='text-2xl m-10' >جدول مدیریت سفارشات</b>
-        <div className={`absolute left-[328px] top-[135px] bg-gray-100 w-36 p-2 pt-1 pb-2 rounded-full`}>
+        <div className={`absolute left-[328px] top-[135px] bg-gray-100 w-[200px] p-2 pt-1 pb-2 rounded-full`}>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="absolute m-1.5 mr-1 bi bi-search" viewBox="0 0 16 16">
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
             </svg>
-            <TextField className='w-4/5 text-base outline-none float-left bg-transparent' placeholder='جستوجو' />
+            <input onChange={filtering} ref={searchElement}  className='w-4/5 text-base outline-none float-left bg-transparent' placeholder='جستوجوی نام خریدار' />
         </div>
-            <select className="absolute left-[480px] top-[135px] inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
-                <option>
-                    Last 30 days
+            <select onChange={filtering} ref={selectElement} className="absolute left-[535px] top-[135px] inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+                <option value='all' selected >
+                    همه سفارشات
+                </option>
+                <option value='true' >
+                    سفارشات تحویل داده شده
+                </option>
+                <option value='false' >
+                    سفارشات در انتظار تحویل
                 </option>
             </select>
-            <div className=" shadow-md sm:rounded-lg w-3/5 h-96 m-auto mt-20">
+            <div className=" shadow-md sm:rounded-lg overflow-x-auto w-3/5 h-96 m-auto mt-20">
     <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead className=" text-xs text-gray-700 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr className="flex" >
@@ -42,16 +73,22 @@ export const OrderManagement = () => {
                 <th scope="col" className="text-center px-6 py-4 w-[20%]">
                     مجموع قیمت
                 </th>
-                <th scope="col" className="text-center px-6 py-4 w-[30%]">
+                <th scope="col" className="text-center px-6 py-4 w-[20%]">
                     زمان ثبت سفارش
                 </th>
                 <th scope="col" className="text-center px-1 py-4 w-[10%]">
                     بررسی سفارش
                 </th>
+                <th scope="col" className="text-center px-1 py-4 w-[10%]">
+                    وضعیت تحویل
+                </th>
             </tr>
         </thead>
         <tbody>
-            {orders.map((order: any) => {
+            {(!ordersArray ? orders : ordersArray).map((order: any) => {
+                const timestamp = order.createdAt;
+                const persianDate = moment(timestamp).format('jYYYY/jM/jD');
+
                 return(
                     <tr className="bg-white flex border-b h-[80px] dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" key={order.id}>
                         <th className="px-6 py-7 w-[40%] text-center font-medium text-gray-900 whitespace-nowrap dark:text-white">
@@ -60,11 +97,18 @@ export const OrderManagement = () => {
                         <td className="px-6 py-7 w-[20%] text-center font-semibold text-gray-900 dark:text-white">
                             {order.prices} تومان
                         </td>
-                        <td className="px-6 py-7 w-[30%] text-center">
-                            {order.createdAt}
+                        <td className="px-6 py-7 w-[20%] text-center">
+                            {persianDate}
                         </td>
                         <td className="px-6 py-7 w-[10%] text-center">
                             <p className="font-medium text-blue-600 dark:text-blue-500 hover:underline cursor-pointer">بررسی</p>
+                        </td>
+                        <td className="px-6 py-7 w-[10%] text-center">
+                            {order.delivered == 'true' ? <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bookmark-check-fill text-green-500 text-center mr-[9px] mt-[5px]" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5zm8.854-9.646a.5.5 0 0 0-.708-.708L7.5 7.793 6.354 6.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z"/>
+                            </svg> : <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bookmark-dash-fill text-red-500 text-center mr-[9px] mt-[5px]" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5zM6 6a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1H6z"/>
+                            </svg>}
                         </td>
                     </tr>
                 )
