9c68b199df6280fe9015674be764a6fd299422cd
Mohammad Hossein Sedarat <mamaddd163@gmail.com>
2023-03-26 00:51:57 +0430

    eddit header navbar

--- src/components/headerLabel/headerLabel.tsx
+++ src/components/headerLabel/headerLabel.tsx
@@ -7,7 +7,7 @@ interface props {
 export const HeaderLabel = (props: props) => {
   return (
         <div className={`bg-black rounded-full text-center w-4/6 m-auto p-1 ${props.className}`} >
-            <p className='text-white' >ارسال رایگان!به ازای خرید بالای 300 هزار تومان</p>
+            <p className='text-white' >ارسال رایگان!به ازای خرید بالای 800 هزار تومان</p>
         </div>
   )
 }
--- src/components/headerNavbar/headerNavbar.tsx
+++ src/components/headerNavbar/headerNavbar.tsx
@@ -40,12 +40,15 @@ export const HeaderNavbar = ({subCategory, setShowNavbar}: myProps) => {
     products?.filter((product: any) => product.subcategory == productSubCategory && !filteredCategories.includes(product.category) && filteredCategories.push(product.category))
 
     return (
-        <div className="relative " onMouseLeave={() => setShowNavbar(null)} >
-            <ul className="flex justify-between w-3/5 m-auto py-[10px] px-[50px]">
-                {filteredCategories.map((route: any) => <li className="cursor-pointer font-bold">
+        <>
+            {filteredCategories.length != 0 && <div className="absolute top-[100px] right-0 z-[1] w-full bg-white" onMouseLeave={() => setShowNavbar(null)} >
+                <ul className="flex justify-between w-3/5 m-auto py-[10px] px-[50px]">
+                    {filteredCategories.map((route: any) => <li className="cursor-pointer font-bold">
                     <Link href={`/products/${productSubCategory}/${route}`}>{route}</Link>
-                </li>)}
-            </ul>
-        </div>
+                    </li>)}
+                </ul>
+            </div>}
+        </>
+        
     )
 }
